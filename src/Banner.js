29632524a3e47f55032fa32cfba674e202e44043
cathjime <xcatherine.jimenez@gmail.com>
2021-10-30 17:50:51 -0400

    initial structure for search

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,37 @@
-import React from "react";
+import { React, useState } from "react";
 import { Button } from "@material-ui/core";
 import "./Banner.css";
+import Search from "./Search.js";
 
 const Banner = () => {
+  const [buttonText, setButtonText] = useState(true);
+
+  const toggleButtonText = () => {
+    setButtonText(!buttonText);
+  };
+
   return (
     <div className="banner">
+      <div className="banner_search">
+        {buttonText && <Search />}
+        {buttonText ? (
+          <Button
+            onClick={toggleButtonText}
+            className="banner_searchbutton"
+            variant="outlined"
+          >
+            Search Dates
+          </Button>
+        ) : (
+          <Button
+            onClick={toggleButtonText}
+            className="banner_searchbutton"
+            variant="outlined"
+          >
+            Hide
+          </Button>
+        )}
+      </div>
       <div className="banner_info">
         <h1>Get out and stretch your imagination</h1>
         <h5>
---
+++ src/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Search.css";
+
+const Search = () => {
+  return (
+    <div className="search">
+      <h2>search</h2>
+    </div>
+  );
+};
+
+export default Search;
