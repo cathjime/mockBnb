46226dc6654a2ef78e7070969e9f5e0d86687001
cathjime <xcatherine.jimenez@gmail.com>
2021-11-14 18:00:38 -0500

    added styling and values for props

--- src/Searchpage.js
+++ src/Searchpage.js
@@ -24,6 +24,25 @@ const SearchPage = () => {
         price="$200 / night"
         total="$1500 total"
       />
+      <SearchResult
+        img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.8YQn-G6toIQZVYbDT4eKJwHaEb%26pid%3DApi&f=1"
+        location="Sunny loft in Sacramento!"
+        title="Loft with plenty of natural light!"
+        description="2 guests ∙ 2 bedrooms ∙ 2 beds ∙ 2.5 bathrooms ∙ WiFi ∙ Kitchen ∙ Washing Machine ∙ close to supermarket"
+        star={4.7}
+        price="$164 / night"
+        total="$984 total"
+      />
+
+      <SearchResult
+        img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2018%2F03%2F49-wisconsin-Top-Airbnb-Rentals-Under-100-via-airbnb.com_.jpg&f=1&nofb=1"
+        location="University Heights"
+        title="Cozy Cambridge Studio!"
+        description="2 guests ∙ 1 bedroom ∙ 1 bed ∙ 1 bathroom ∙ WiFi ∙ Kitchen ∙ Washing Machine ∙ blocks from Main Street"
+        star={4.5}
+        price="$87 / night"
+        total="$555 total"
+      />
     </div>
   );
 };
